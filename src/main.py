4468a15be02f8a0c7a33c0a528ba3e6e697febf9
Seung Jun Lee <mps86@naver.com>
2024-03-02 15:52:11 +0900

    주석 제거, Students 클래스 repr PEP8 반영

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
@@ -7,31 +6,6 @@ from sqlalchemy.orm import Session
 from database import get_database
 from database_orm import Students
 
-# from fastapi.middleware.cors import CORSMiddleware
-
-# import config
-# from apis.common import common_router
-# from apis.posts import post_router
-# from database import close_db, create_db_and_tables
-
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await create_db_and_tables()
-#     yield
-#     await close_db()
-
-# app = FastAPI(lifespan=lifespan)
-
-# app.include_router(common_router)
-# app.include_router(post_router)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=config.cors.origins.split(","),
-#     allow_credentials=True,
-#     allow_methods=config.cors.methods.split(","),
-#     allow_headers=config.cors.headers.split(","),
-# )
 
 app = FastAPI()
 
