4ea6ef26b7e76961ce73483451cb046513217928
Seung Jun Lee <mps86@naver.com>
2024-03-25 03:08:25 +0900

    리팩토링 - pep8 auto formatting

--- src/abstract/repository.py
+++ src/abstract/repository.py
@@ -7,32 +7,27 @@ from database.database import get_database
 
 T = TypeVar('T')
 
-class Repository(ABC, Generic[T]):
 
+class Repository(ABC, Generic[T]):
 
     def __init__(self, session: Session = Depends(get_database)):
         self.session = session
 
-    
     @abstractmethod
     def get_all_entities(self) -> List[T]:
         pass
 
-    
     @abstractmethod
     def get_entity_by_id(self, id) -> T | None:
         pass
-
 
     @abstractmethod
     def create_entity(self, entity: T) -> T:
         pass
 
-
     @abstractmethod
     def update_entity_by_id(self, id, request) -> T:
         pass
-
 
     @abstractmethod
     def delete_entity_by_id(self, id) -> T:
