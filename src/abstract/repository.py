c1e1e589563b6611d1703cc9e558b94d36dedf99
Seung Jun Lee <mps86@naver.com>
2024-04-02 22:40:35 +0900

    추상화 타입 힌팅을 더 명확하게 하기 위한 타입 힌팅 변수이름 수정

--- src/abstract/repository.py
+++ src/abstract/repository.py
@@ -5,30 +5,32 @@ from typing import List, TypeVar, Generic
 
 from database.database import get_database
 
+EducationalEntity = TypeVar('EducationalEntity')
+Request = TypeVar('Request')
 T = TypeVar('T')
 
 
-class Repository(ABC, Generic[T]):
+class Repository(ABC, Generic[EducationalEntity]):
 
     def __init__(self, session: Session = Depends(get_database)):
         self.session = session
 
     @abstractmethod
-    def get_all_entities(self) -> List[T]:
+    def get_all_entities(self) -> List[EducationalEntity]:
         pass
 
     @abstractmethod
-    def get_entity_by_id(self, id) -> T | None:
+    def get_entity_by_id(self, id: T) -> EducationalEntity | None:
         pass
 
     @abstractmethod
-    def create_entity(self, entity: T) -> T:
+    def create_entity(self, entity: EducationalEntity) -> EducationalEntity:
         pass
 
     @abstractmethod
-    def update_entity_by_id(self, id, request) -> T:
+    def update_entity_by_id(self, id: T, request) -> EducationalEntity:
         pass
 
     @abstractmethod
-    def delete_entity_by_id(self, id) -> T:
+    def delete_entity_by_id(self, id: T) -> EducationalEntity:
         pass
