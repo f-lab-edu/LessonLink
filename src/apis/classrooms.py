3664c001f4b901eee53347c947df330da384b6e9
Seung Jun Lee <mps86@naver.com>
2024-04-11 21:32:05 +0900

    기능추가 - classroom endpoint 권한 설정

--- src/apis/classrooms.py
+++ src/apis/classrooms.py
@@ -4,19 +4,27 @@ from database.ClassroomsRepository import ClassroomsRepository
 from schema.request import CreateClassroomRequest, UpdateClassroomRequest
 from database.database_orm import Classrooms
 from schema.response import ClassroomSchema
+from functions.security import get_access_token
+from functions.student import StudentFunction
 
 
 router = APIRouter(prefix="/classrooms")
 
 
 @router.get("/", status_code=200, tags=["Classrooms"])
-def get_classroom_handler(repo: ClassroomsRepository = Depends()):
+def get_classroom_handler(
+    access_token: str = Depends(get_access_token),
+    student_func: StudentFunction = Depends(),
+    repo: ClassroomsRepository = Depends()
+):
     return repo.get_all_entities()
 
 
 @router.get("/{id}", status_code=200, tags=["Classrooms"])
 def get_classroom_by_id_handler(
     id: int,
+    access_token: str = Depends(get_access_token),
+    student_func: StudentFunction = Depends(),
     repo: ClassroomsRepository = Depends()
 ):
     classroom = repo.get_entity_by_id(id=id)
@@ -31,8 +39,17 @@ def get_classroom_by_id_handler(
 @router.post("/", status_code=201, tags=["Classrooms"])
 def post_create_classroom_handler(
     request: CreateClassroomRequest,
+    access_token: str = Depends(get_access_token),
+    student_func: StudentFunction = Depends(),
     repo: ClassroomsRepository = Depends()
 ):
+
+    payload: dict = student_func.decode_jwt(access_token=access_token)
+    role: str = payload['role']
+    if not (role == 'admin'):
+        raise HTTPException(
+            status_code=401, detail=f"Admin only allowed.")
+
     classroom: Classrooms = Classrooms.create(request=request)
     classroom: Classrooms = repo.create_entity(classroom=classroom)
     return ClassroomSchema.from_orm(classroom)
@@ -42,8 +59,17 @@ def post_create_classroom_handler(
 def patch_classroom_handler(
     id: int,
     request: UpdateClassroomRequest,
+    access_token: str = Depends(get_access_token),
+    student_func: StudentFunction = Depends(),
     repo: ClassroomsRepository = Depends()
 ):
+
+    payload: dict = student_func.decode_jwt(access_token=access_token)
+    role: str = payload['role']
+    if not (role == 'admin'):
+        raise HTTPException(
+            status_code=401, detail=f"Admin only allowed.")
+
     classroom = repo.get_entity_by_id(id=id)
 
     if classroom:
@@ -55,8 +81,17 @@ def patch_classroom_handler(
 @router.delete("/{id}", status_code=204, tags=["Classrooms"])
 def delete_classroom_handler(
     id: int,
+    access_token: str = Depends(get_access_token),
+    student_func: StudentFunction = Depends(),
     repo: ClassroomsRepository = Depends()
 ):
+
+    payload: dict = student_func.decode_jwt(access_token=access_token)
+    role: str = payload['role']
+    if not (role == 'admin'):
+        raise HTTPException(
+            status_code=401, detail=f"Admin only allowed.")
+
     classroom = repo.get_entity_by_id(id=id)
 
     if classroom:
