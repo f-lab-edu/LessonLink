903e673238e697041638525173878a177abefc62
Seung Jun Lee <mps86@naver.com>
2024-04-15 13:49:10 +0900

    pytest - schedule, reservation 롤백

--- src/tests/test_api_reservations.py
+++ src/tests/test_api_reservations.py
@@ -36,6 +36,18 @@ def test_post_create_reservation_handler_student(
         "Authorization": f"Bearer {access_token}"
     }
 
+    request_body = {
+        "student_id": get_init_config_data('test_account', 'STUDENT_ID'),
+        "schedule_id": 42,
+        "reservated_date": "2024-04-15",
+        "reservated_time": "04:46:38.834Z",
+        "status": "reservated",
+        "notes": "string"
+    }
+
+    response = client.post("/reservations", headers=headers)
+    assert response.status_code == 200
+
 
 def test_post_create_reservation_handler_instructor(
     client, instructor_credentials, login
@@ -59,38 +71,3 @@ def test_delete_reservation_handler(
     client, student_credentials, instructor_credentials, login
 ):
     pass
-
-
-def test_delete_schedule_handler_student(
-    client, student_credentials, login
-):
-    access_token = test_post_student_login_handler(
-        student_credentials, login
-    )
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    schedule_id = get_init_config_data("schedules", "id")
-
-    response = client.delete(f"/schedules/{schedule_id}", headers=headers)
-    assert response.status_code == 401
-
-
-def test_delete_schedule_handler_instructor(
-    client, instructor_credentials, login
-):
-    access_token = test_post_instructor_login_handler(
-        instructor_credentials, login
-    )
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    schedule_id = get_init_config_data("schedules", "id")
-
-    response = client.delete(f"/schedules/{schedule_id}", headers=headers)
-    assert response.status_code == 204
-
-    response = client.get(f"/schedules/{schedule_id}", headers=headers)
-    assert response.status_code == 404
