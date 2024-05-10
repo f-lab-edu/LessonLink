b8a16859705007e9822e70537e3aa69ba9efb014
zbnerd <mps86@naver.com>
2024-05-10 14:52:04 +0900

    async - course endpoint

--- src/database/database_init.py
+++ src/database/database_init.py
@@ -1,11 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL: str = "mysql+pymysql://root:admin@localhost:3306/lessonlink"
+DATABASE_URL_ASYNC: str = "mysql+aiomysql://root:admin@localhost:3306/lessonlink"
 
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+async_engine = create_async_engine(DATABASE_URL_ASYNC, echo=True, future=True)
+AsyncSessionFactory = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=async_engine,
+    class_=AsyncSession
+)
 
 
 def get_database():
@@ -14,3 +25,8 @@ def get_database():
         yield session
     finally:
         session.close()
+
+
+async def get_async_database():
+    async with AsyncSessionFactory() as session:
+        yield session
--- src/tests/test_api_courses_large_data.py
+++ src/tests/test_api_courses_large_data.py
@@ -15,7 +15,7 @@ def test_post_create_course_handler_large_data(
         "Authorization": f"Bearer {access_token}"
     }
 
-    for i in range(1000000, 1001000):
+    for i in range(2147013242, 2147100000):
         request_body = {
             "id": i,
             "name": secrets.token_urlsafe(8),
@@ -23,54 +23,8 @@ def test_post_create_course_handler_large_data(
             "start_date": "2024-04-11",
             "end_date": "2024-04-11",
             "instructor_id": id,
-            "cost": 500000
+            "cost": secrets.randbelow(500000)
         }
 
         response = client.post("/courses", headers=headers, json=request_body)
         assert response.status_code == 201
-
-def test_patch_course_handler_instructor_large_data(
-    client, credentials, login_get_access_token
-):
-
-    # 강사 권한
-    access_token = test_post_instructor_login_handler(
-        credentials, login_get_access_token
-    )
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    for i in range(1000000, 1001000):
-
-        id = get_init_config_data('test_account', 'instructor2_ID')
-        request_body = {
-            "id": i,
-            "name": secrets.token_urlsafe(8),
-            "description": secrets.token_urlsafe(8),
-            "start_date": "2024-04-10",
-            "end_date": "2024-04-10",
-            "instructor_id": id,
-            "cost": 200000
-        }
-
-        response = client.patch(f"/courses/{i}",
-                                headers=headers, json=request_body)
-        assert response.status_code == 200
-
-
-def test_delete_course_handler_instructor_large_data(
-    client, credentials, login_get_access_token
-):
-        # 강사 권한
-    access_token = test_post_instructor_login_handler(
-        credentials, login_get_access_token
-    )
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    
-    for i in range(1000000, 1001000):
-
-        response = client.delete(f"/courses/{i}", headers=headers)
-        assert response.status_code == 204
