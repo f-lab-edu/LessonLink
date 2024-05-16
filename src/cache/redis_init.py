b42a2be283adc2b33c1b75c6e7de29957ae6c751
zbnerd <mps86@naver.com>
2024-05-16 20:20:33 +0900

    redis - init 수정

--- src/cache/redis_init.py
+++ src/cache/redis_init.py
@@ -1,4 +1,4 @@
 import redis
 
-pool = redis.ConnectionPool(host="127.0.0.1", port=6379, db=0)
+pool = redis.ConnectionPool(host="127.0.0.1", port=6379, db=0, encoding="UTF-8", decode_responses=True)
 redis_client = redis.Redis(connection_pool=pool)
