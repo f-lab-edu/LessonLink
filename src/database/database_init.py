67c850d5bb89a79cefa694cd054d565b12a96563
zbnerd <mps86@naver.com>
2024-05-16 20:14:40 +0900

    localhost를 루프백주소로 수정

--- src/database/database_init.py
+++ src/database/database_init.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL: str = "mysql+pymysql://root:admin@localhost:3306/lessonlink"
+DATABASE_URL: str = "mysql+pymysql://root:admin@127.0.0.1:3306/lessonlink"
 
 
-engine = create_engine(DATABASE_URL, echo=True, future=True)
+engine = create_engine(
+    DATABASE_URL, future=True,
+    pool_size=500, max_overflow=500, pool_timeout=30, pool_recycle=3600,
+)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
